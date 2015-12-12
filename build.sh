d51b3675586092303e73a9f772c2fef487c28eb7
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2015-12-12 11:24:13 +0100

    build release against zinc master

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-export LD_LIBRARY_PATH=`pwd`/../rust/lib
-export PATH=`pwd`/../rust/bin:$PATH
 export TARGET=thumbv7m-none-eabi
 export PLATFORM=lpc17xx
 
 rustc --version
-cargo build --target=$TARGET --features "mcu_$PLATFORM" && cp ./target/thumbv7m-none-eabi/debug/rustyfridge rustyfridge.axf
+cargo build --target=$TARGET --features "mcu_$PLATFORM" --release
