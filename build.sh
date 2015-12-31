6c264680956b7ad71d6e75688a0103b243ceb0f4
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2015-12-31 13:01:01 +0100

    verbose builds

--- build.sh
+++ build.sh
@@ -1,5 +1,1 @@
-export TARGET=thumbv7m-none-eabi
-export PLATFORM=lpc17xx
-
-rustc --version
-cargo build --target=$TARGET --features "mcu_$PLATFORM" --release
+cargo build --target=thumbv7m-none-eabi --features "mcu_lpc17xx" --release --verbose
