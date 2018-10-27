f37ff6c8aaa7cab66afebe1ee179ecb312af720a
Felix Obenhuber <felix.obenhuber@esrlabs.com>
2018-10-27 18:08:00 +0200

    Play with tock

--- src/main.rs
+++ src/main.rs
@@ -1,230 +1,83 @@
-// Copyright (C) 2016 Felix Obenhuber
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+#![feature(alloc)]
 #![no_std]
-#![feature(start, plugin, core_intrinsics)]
-#![plugin(macro_platformtree)]
 
-extern crate zinc;
+extern crate alloc;
+extern crate tock;
 
-use zinc::drivers::chario::CharIO;
-use zinc::hal::pin::{Adc, Gpio};
-use zinc::hal::timer::Timer;
+use alloc::string::String;
+use tock::console::Console;
+use tock::led;
+use tock::syscalls;
+use tock::timer;
+use tock::timer::Duration;
 
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-const TEMP_LOW: i32 = 5000;
-const TEMP_MID: i32 = 10000;
-const TEMP_HIGH: i32 = 15000;
-const CONTROL_HYSTERESIS: i32 = 1000;
-const ACTUAL_FACTOR: i32 = 10;
-const ACTUAL_ALLOWED_DIFF: i32 = 250;
-const ACTUAL_ALLOWED_FAILS: u32 = 10;
-const SETPOINT_FACTOR: i32 = 10;
-const SETPOINT_ALLOWED_DIFF: i32 = 250;
-const SETPOINT_ALLOWED_FAILS: u32 = 10;
+const ADC_DRIVER: usize = 0x00005;
+const SUBSCRIBE: usize = 0;
+const SAMPLE: usize = 1;
 
-platformtree!(
-    lpc17xx@mcu {
-        clock {
-            source = "main-oscillator";
-            source_frequency = 12_000_000;
-            pll {
-                m = 50;
-                n = 3;
-                divisor = 4;
-            }
-        }
-
-        timer {
-            timer@1 {
-                counter = 25;
-                divisor = 4;
-            }
-        }
-
-        uart {
-            uart@0 {
-                baud_rate = 115200;
-                mode = "8N1";
-                tx = &uart_tx;
-                rx = &uart_rx;
-            }
-        }
-
-        gpio {
-            0 {
-                uart_tx@2;
-                uart_rx@3;
-                compressor@8 { direction = "out"; }
-                led@22 { direction = "out"; }
-                adc0@23 { direction = "out"; function = "ad0_0"; }
-                adc2@25 { direction = "out"; function = "ad0_2"; }
-            }
-        }
+fn log(s: &str, v: Option<i32>) {
+    let mut c = Console::new();
+    c.write(String::from(s));
+    if let Some(v) = v {
+        c.write(String::from(" "));
+        c.write(tock::fmt::i32_as_decimal(v as i32));
     }
-os {
-    single_task {
-        loop = "run";
-        args {
-            compressor = &compressor;
-            actual = &adc2;
-            led = &led;
-            setpoint = &adc0;
-            timer = &timer;
-            uart = &uart;
-        }
-    }
-}
-);
-
-fn run(args: &pt::run_args) {
-    args.uart.puts("Rustyfridge ");
-    args.uart.puts(VERSION);
-    args.uart.puts("\r\n");
-
-    args.timer.wait_ms(2000);
-
-    let mut loops: u32 = 0;
-    let mut actual_filter = Filter::new(ACTUAL_FACTOR, ACTUAL_ALLOWED_DIFF, ACTUAL_ALLOWED_FAILS);
-    let mut setpoint_filter = Filter::new(SETPOINT_FACTOR,
-                                          SETPOINT_ALLOWED_DIFF,
-                                          SETPOINT_ALLOWED_FAILS);
-    let mut cool: bool = false;
-
-    loop {
-        // Actual value needs a fixed offset and factor for conversion to mdeg
-        let actual = actual_filter.filter(args.actual.read() as i32) * 100 - 4000;
-
-        // Setpoint matches log poti in three ranges
-        let setpoint = match setpoint_filter.filter(args.setpoint.read() as i32) {
-            0...180 => TEMP_LOW,
-            181...660 => TEMP_MID,
-            _ => TEMP_HIGH,
-        };
-
-        // Decide whether to cool or not
-        cool = if (actual - setpoint).abs() > CONTROL_HYSTERESIS {
-            actual > setpoint
-        } else {
-            cool
-        };
-        if cool {
-            args.compressor.set_high();
-        } else {
-            args.compressor.set_low();
-        }
-
-        // Blink faster if compressor is running
-        if loops &
-           if cool {
-            0x4
-        } else {
-            0x8
-        } != 0 {
-            args.led.set_high();
-        } else {
-            args.led.set_low();
-        }
-
-        // Basic formatting
-        let print_deg = |value: i32| {
-            if value > -100000 && value < 100000 {
-                args.uart.puts(" ");
-            }
-            if value > -10000 && value < 10000 {
-                args.uart.puts(" ");
-            }
-            let v = if value < 0 {
-                args.uart.puts("-");
-                -value as u32
-            } else {
-                args.uart.puts(" ");
-                value as u32
-            };
-            args.uart.puti(v / 1000);
-            args.uart.puts(".");
-            args.uart.puti((v % 1000) / 100);
-        };
-
-        // Trace current state
-        args.uart.puts(if cool {
-            "❄ "
-        } else {
-            "  "
-        });
-        print_deg(actual);
-        args.uart.puts(" | ");
-        print_deg(setpoint);
-        args.uart.puts(" | diff: ");
-        print_deg(setpoint - actual);
-        args.uart.puts("\r\n");
-
-        // Update loop counter
-        if loops == u32::max_value() {
-            loops = 0;
-        } else {
-            loops += 1;
-        }
-
-        args.timer.wait_ms(100);
-    }
+    c.write(String::from("\n"));
 }
 
-struct Filter {
-    last: Option<i32>,
-    factor: i32,
-    max_difference: i32,
-    current_fails: u32,
-    allowed_fails: u32,
+fn sample(channel: usize) -> isize {
+    unsafe { syscalls::command(ADC_DRIVER, SAMPLE, channel, 0) }
 }
 
-impl Filter {
-    fn new(factor: i32, max_difference: i32, allowed_fails: u32) -> Filter {
-        assert!(factor >= 2);
-        assert!(allowed_fails >= 2);
-        Filter {
-            last: None,
-            factor: factor,
-            max_difference: max_difference,
-            current_fails: allowed_fails,
-            allowed_fails: allowed_fails,
-        }
-    }
+use core::convert::From;
+use core::ops::{Add, Mul};
 
-    fn filter(&mut self, value: i32) -> i32 {
-        let next = match self.last {
-            Some(last) => {
-                if (last - value).abs() < self.max_difference {
-                    self.current_fails = 0;
-                    value
-                } else {
-                    if self.current_fails <= self.allowed_fails {
-                        self.current_fails += 1;
-                        last
-                    } else {
-                        self.current_fails = 0;
-                        value
-                    }
+#[derive(Default)]
+struct FloatMean<T>(Option<T>);
+
+impl<T> FloatMean<T>
+where
+    T: Mul<i32> + Add<T, Output = T> + From<i32> + Mul<T, Output = T>,
+    T: Add<T>,
+{
+    fn add(&mut self, other: T) {
+        match self.0 {
+            Some(v) => {
+                self.0 = {
+                    let a = v * 19;
+                    let b = a + other;
+                    Some(b / 20)
                 }
             }
-            None => value,
-        };
+            None => self.0 = Some(other),
+        }
+    }
 
-        self.last = match self.last {
-            Some(v) => Some((v * (self.factor - 1) + next) / self.factor),
-            None => Some(next),
-        };
-        self.last.unwrap()
+    fn value(&self) -> T {
+        self.0.unwrap_or(0.into())
+    }
+}
+
+fn main() {
+    let mut setpoint = FloatMean::default();
+    let mut current = FloatMean::default();
+
+    let mut cb = |_, c, v| {
+        led::get(0).unwrap().toggle();
+        if c == 0 {
+            setpoint.add(v as i32);
+            sample(1);
+        } else {
+            current.add(v as i32);
+            log("Setpoint", Some(setpoint.value()));
+            log("Current", Some(current.value()));
+        }
+    };
+    let _subscription = syscalls::subscribe(ADC_DRIVER, SUBSCRIBE, &mut cb).unwrap();
+
+    loop {
+        sample(0);
+        led::get(1).unwrap().toggle();
+        timer::sleep(Duration::from_ms(100));
     }
 }
--- support/t32/t32.sh
+++
@@ -1,1 +0,0 @@
-t32marm-qt -c config.t32,main.cmm 10101 LPC17xx USB NODE=ARM
